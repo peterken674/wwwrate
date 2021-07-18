643a9b9ddf21de15c74995d0dbd57e9eb269d238
Peter Kennedy <peter.kimani@student.moringaschool.com>
2021-07-18 22:45:37 +0300

    Add profile pic field to profile model.

--- wwwrate/models.py
+++ wwwrate/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.forms.models import modelform_factory
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    profile_pic = CloudinaryField('images', default='image/upload/v1626430054/default_zogkvr.png')
     role = models.CharField(max_length=150)
     github = models.CharField(max_length=200, blank=True)
     linkedin = models.CharField(max_length=200, blank=True)
